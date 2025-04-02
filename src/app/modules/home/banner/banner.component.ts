80ff16fb90bbe7fcb35327ccd0824d9bbb0a9935
Chandra gupt sharma <developer8@cashpor.in>
2025-04-02 18:11:43 +0530

    navbar

--- src/app/modules/home/banner/banner.component.ts
+++ src/app/modules/home/banner/banner.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-
+interface Slide {
+  image: string;
+  title: string;
+  description: string;
+  classes?: { [key: string]: boolean }; // Optional `classes` property
+}
 @Component({
   selector: 'app-banner',
   standalone: false,
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  styleUrl: './banner.component.scss',
 })
 export class BannerComponent {
-
+  slides = [
+    {
+      image:
+        'https://blog.g3fashion.com/wp-content/uploads/2022/06/mens-kurta-styles.jpg',
+    },
+    {
+      image:
+        'https://m.media-amazon.com/images/S/aplus-media-library-service-media/5b091f7f-dea1-4aee-b5a9-2c621e78f29e.__CR0,0,970,600_PT0_SX970_V1___.png',
+    },
+    {
+      image:
+        'https://www.bonsoir.co.in/cdn/shop/articles/Latest_Kurta_Pajama_Sets_for_Diwali.jpg?v=1663581986',
+    },
+  ];
 }
--- src/app/modules/home/header/header.component.ts
+++ src/app/modules/home/header/header.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  // Make sure menuOpen is set to false initially to keep the menu hidden
+  menuOpen = false;
 
+  // Toggle the state of menuOpen when the hamburger icon is clicked
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
