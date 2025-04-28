457c62cf01bdce8457acb4f874aa916d4beb91ff
chandraguptsharma1 <chandraguptsharma1@gmail.com>
2025-04-28 22:28:04 +0530

    push to live

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://3g-management-backend.vercel.app/api',
-  baseUrl: 'https://threeg-management-backend.onrender.com/api',
+   baseUrl: 'https://desinaar-backend.onrender.com/api',
 };
