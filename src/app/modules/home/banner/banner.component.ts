26b6cb34f4980da88e053f9ca2c823e3109322e4
chandraguptsharma1 <chandraguptsharma1@gmail.com>
2026-04-14 18:25:39 +0530

    working on look

--- src/app/modules/home/banner/banner.component.ts
+++ src/app/modules/home/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
@@ -7,7 +8,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrl: './banner.component.scss',
 })
 export class BannerComponent implements OnInit, OnDestroy {
-  activeIndex: number = 0;
+
+  activeIndex = 0;
+  isPlaying = false;
   private autoPlayInterval: any;
 
   carouselItems = [
@@ -17,6 +20,8 @@ export class BannerComponent implements OnInit, OnDestroy {
       desktopTitle: 'New Collection',
       mobileTitle: 'New Arrivals',
       buttonText: 'Explore Now',
+      route: '/new_arrival',
+      loaded: false
     },
     {
       desktopImage: 'https://res.cloudinary.com/dcrcp8a3d/image/upload/v1774517383/mens-festive-kurta-collection-india_eop4yj.png',
@@ -24,6 +29,8 @@ export class BannerComponent implements OnInit, OnDestroy {
       desktopTitle: 'Festive Styles',
       mobileTitle: 'Festive Looks',
       buttonText: 'Shop Festive',
+      route: '/category',
+      loaded: false
     },
     {
       desktopImage: 'https://res.cloudinary.com/dcrcp8a3d/image/upload/v1774517361/designer-embroidered-kurta-for-men_bve76k.png',
@@ -31,8 +38,12 @@ export class BannerComponent implements OnInit, OnDestroy {
       desktopTitle: 'Elegant Ethnicwear',
       mobileTitle: 'Ethnic Looks',
       buttonText: 'View Collection',
+      route: '/products',
+      loaded: false
     },
   ];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.startAutoPlay();
@@ -43,7 +54,8 @@ export class BannerComponent implements OnInit, OnDestroy {
   }
 
   startAutoPlay(): void {
-    this.pauseAutoPlay(); // double interval se bachao
+    this.pauseAutoPlay();
+    this.isPlaying = true;
     this.autoPlayInterval = setInterval(() => {
       this.nextSlide();
     }, 3000);
@@ -54,18 +66,33 @@ export class BannerComponent implements OnInit, OnDestroy {
       clearInterval(this.autoPlayInterval);
       this.autoPlayInterval = null;
     }
+    this.isPlaying = false;
   }
 
   nextSlide(): void {
     this.activeIndex = (this.activeIndex + 1) % this.carouselItems.length;
+    this.resetProgress();
   }
 
   prevSlide(): void {
-    this.activeIndex =
-      (this.activeIndex - 1 + this.carouselItems.length) % this.carouselItems.length;
+    this.activeIndex = (this.activeIndex - 1 + this.carouselItems.length) % this.carouselItems.length;
+    this.resetProgress();
   }
 
   goToSlide(index: number): void {
     this.activeIndex = index;
+    this.resetProgress();
+  }
+
+  resetProgress(): void {
+    // Progress bar restart karo
+    this.isPlaying = false;
+    setTimeout(() => { this.isPlaying = true; }, 10);
+  }
+
+  onBannerClick(item: any): void {
+    if (item.route) {
+      this.router.navigate([item.route]);
+    }
   }
 }
